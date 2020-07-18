a94ebd2d512e604f21c7f4522ed1ad874023fd1a
Kaixinguo <kaixinguo@kaixinguo.site>
2020-07-18 05:43:05 -0700

    Add install/remove script to bash config

---
+++ data/config/bash/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dir=$(dirname $(realpath $0))
+cd $dir
+
+# Create Soft Link
+files='.bashrc .bashrc.d .profile'
+for file in ${files[@]}
+do
+    rm -f $HOME/$file
+    ln -s $(realpath $dir/$file) $HOME
+done
+
+# Create .bin/local
+mkdir -p $HOME/.bin/local
+rm -f .bashrc.d/local
+ln -sf $(realpath $HOME/.bin/local) .bashrc.d/local
---
+++ data/config/bash/remove.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $(dirname $(realpath $0))
+
+# Remove Soft Link
+files='.bashrc .bashrc.d .profile'
+for file in ${files[@]}
+do
+    rm -f $HOME/$file
+done
+
+# Remove .bin/local
+rm -f .bashrc.d/local
