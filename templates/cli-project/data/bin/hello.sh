4de8ba9c97db1e17fd5e66a4bfa7808784b51c23
Kaixinguo <kaixinguo@kaixinguo.site>
2020-10-03 02:17:51 +0800

    Optimize template: cli-project

--- templates/cli-project/data/bin/hello.sh
+++ templates/cli-project/data/bin/hello.sh
@@ -3,5 +3,12 @@
 #u:Usage: myexample hello
 
 echo "Hello,world!"
-echo "[INFO] MYEXAMPLE_HOME=$MYEXAMPLE_HOME"
-echo "[INFO] MYEXAMPLE_CMD=$MYEXAMPLE_CMD"
+
+set_log_level 4
+debug "MYEXAMPLE_HOME=$MYEXAMPLE_HOME"
+debug "MYEXAMPLE_CMD=$MYEXAMPLE_CMD"
+
+log "\033[32mTest text\033[0m"
+error "Test text"
+warn "Test text"
+debug "Test text"
--- templates/cli-project/data/lib/00-log-utils.sh
+++ templates/cli-project/data/lib/00-log-utils.sh
@@ -1,11 +1,45 @@
 # Log Utils #
 
-# Usage: log_error "ERROR_MESSAGE"
-log_error() { [ "$LOG_LEVEL" -ge 0 ] && echo "[ERROR] $*" >&2; }
+# Set default log level
+LOG_LEVEL=${LOG_LEVEL:-3}
 
-# Usage: log_info "INFO_MESSAGE"
-log_info() { [ "$LOG_LEVEL" -ge 1 ] && echo "[INFO] $*" >&2; }
+# Usage: set_log_level LOG_LEVEL
+set_log_level() {
+    LOG_LEVEL=${1:-1}
+    debug "Set LOG_LEVEL=$LOG_LEVEL"
+}
 
-# Usage: log_debug "DEBUG_MESSAGE"
-log_debug() { [ "$LOG_LEVEL" -ge 2 ] && echo "[DEBUG] $*" >&2; }
+# Usage: LEVEL=n log "MESSAGE" [LEVEL=2] [FLAG=INFO]
+log() {
+    local msg="$1"
+    local level=${2:-2};
+    local flag=${3:-INFO}
+
+    [ -n "$LEVEL" ] \
+        && [ "$LEVEL" -ne "$level" ] \
+        && level=$LEVEL \
+        && flag="$flag:$LEVEL"
+
+    [ "$LOG_LEVEL" -ge "$level" ] \
+        && printf "[$flag] $msg\n";
+}
+
+# Usage: error "ERROR_MESSAGE" [LEVEL=1] [FLAG=ERROR]
+error() {
+    printf '\033[31m' >&2;
+    log "$1" ${2:-1} ${3:-ERROR} >&2;
+    printf '\033[0m' >&2;
+}
+
+# Usage: warn "WARN_MESSAGE" [LEVEL=3] [FLAG=WARN]
+warn() {
+    printf '\033[33m' >&2;
+    log "$1" ${2:-3} ${3:-WARN} >&2;
+    printf '\033[0m' >&2;
+}
+
+# Usage: debug "DEBUG_MESSAGE" [LEVEL=4] [FLAG=DEBUG]
+debug() {
+    log "$*" ${2:-4} "\033[34m${3:-DEBUG}\033[0m";
+}
 
